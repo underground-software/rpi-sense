1bfd86a64e437b16d14ddc4ba5cfe0c564e516b9
Charles Mirabile <cmirabil@redhat.com>
2022-04-12 14:48:47 -0400

    annotate the code

--- main.c
+++ main.c
@@ -28,13 +28,14 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/twi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+//macros for what outputs are connected to what pins
 #define EE_WP			(1 << PB0)
 #define KEYS_INT		(1 << PB6)
 #define FRAME_INT		(1 << PB7)
@@ -45,20 +46,24 @@
 #define LED_SDI			(1 << PC3)
 #define LED_OE_N		(1 << PC7)
 
+//unused macros for setting and clearing bits in ports
 #define set(port,x) port |= (x)
 #define clr(port,x) port &= ~(x)
 
+//extern decls for functions written in assembly
 extern void draw_loop();
 extern void clear_gain(void);
 extern void delay(uint8_t ticks);
 extern void write_data(uint32_t data, char type);
 
+//unused / incorrect enum for register addresses
 enum REG_ADDR {
 	REG_ID = 192,
 	REG_CFG_LOW,
 	REG_CFG_HIGH,
 };
 
+//unused enum of pulse lenghts for communicating with LED driver
 typedef enum {
 	DAT_LATCH = 22,
 	CONF_WRITE = 20,
@@ -71,6 +76,8 @@ typedef enum {
 	THERM_READ = 10,
 } le_key;
 
+//pixel data array - each row is 8 pixels R then G then B values
+//initial state is the rainbow pattern seen at bootup
 volatile uint8_t pixels[] = {
 	0x1F, 0x1F, 0x1F, 0x1F, 0x14, 0x03, 0x00, 0x00,
 	0x00, 0x00, 0x03, 0x12, 0x1F, 0x1F, 0x1F, 0x1F,
@@ -110,20 +117,31 @@ volatile char i2c_reg = 0xff;
 
 int main(void)
 {
+	//configure initial state of i/o pins
 	PORTA = 0;
 	PORTB = 1;
 	PORTC = 0;
 	PORTD = 0xFF;
+	//configure which pins are inputs and which are outputs
 	DDRB = EE_WP | FRAME_INT | KEYS_INT;
 	DDRC = LED_SDI | LED_CLKR | LED_LE | LED_OE_N;
 	DDRD = 0xFF;
 
+	//this line is buggy as it mixes assigning to timer/counter 0 control register A
+	//with a macro (CS12) that specifies a bit position in control register B. I think
+	//this basically does nothing as the bit position of CS12 maps to a unused bit in reg A
 	TCCR0A = (1<<CS12);
+	//slow down the i2c bit rate
 	TWBR = 0xff;
+	//set address to 0x46
 	TWAR = 0x46 << 1;
+	//enable TWI, and make the twi system aknowledge its address and fire interrupts for twi
 	TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT) | (1 << TWIE);
 	//clear_gain();
+	//enable interrupts
 	sei();
+	//jump to assembly code
 	draw_loop();
+	//we sholdn't be able to get here since draw_loop never returns, but this for loop handles the case that it does
 	for(;;);
 }
